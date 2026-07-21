8bc730bd7c0c0d123f78d41de04ce8255481a333
showbiz0 <samuele8max@gmail.com>
2026-07-21 21:36:43 +0200

    Edit site config, gallery images, contact form redesign, history and our strengths improvements, changed projects content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://showbiz0.github.io",
-  base: "/mora-display",
+  site: "https://moradisplay.com",
   vite: {
     plugins: [tailwindcss()],
   },
